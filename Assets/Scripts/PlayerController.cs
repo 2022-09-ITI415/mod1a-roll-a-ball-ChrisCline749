b6634ce7db15ce8375503ea936d745805fc07663
ChrisCline749 <46876667+ChrisCline749@users.noreply.github.com>
2022-09-15 01:05:20 -0400

    Player Movement
    Added player movement through a Player Inputs component and a c# script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+
+    private Rigidbody rb;
+    private float moveX;
+    private float moveY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue movementValue)
     {
-        
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        moveX = movementVector.x;
+        moveY = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveX, 0.0f, moveY);
+
+        rb.AddForce(movement * speed);
     }
 }
